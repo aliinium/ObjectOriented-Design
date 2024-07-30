15a11f189827958508a546012adf7171d6bcceee
Ali <62478970+aliinium@users.noreply.github.com>
2024-07-30 12:46:49 +0330

    add point class to ex02 and write codes

--- exercises/Ex02/Point.cs
+++ exercises/Ex02/Point.cs
@@ -1,6 +1,22 @@
-﻿namespace Ex02;
+﻿using System;
+using static System.Math;
+
+namespace Ex02;
 
 public class Point
 {
+    public int X, Y;
 
+    public double DistanceFromSource() 
+    {
+        return Math.Sqrt(this.X * this.X + this.Y * this.Y);
+    }
+
+    public double DistanceFromPoint(Point p)
+    {
+        int dx = this.X - p.X;
+        int dy = this.Y - p.Y;
+
+        return Math.Sqrt(dx * dx + dy * dy);
+    }
 }
--- exercises/Ex02/Program.cs
+++ exercises/Ex02/Program.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
 
+namespace Ex02;
+
 public class SecondExercise
 {
     public static void Main(string[] args)
     {
-        
+        Point p1 = new Point();
+        p1.X = 10;
+        p1.Y = 10;
+
+        Point p2 = new Point();
+        p2.X = 20;
+        p2.Y = 20;
+
+        Console.WriteLine("Point1 Distance From 0,0: \t" + p1.DistanceFromSource().ToString());
+        Console.WriteLine("Point2 Distance From 0,0: \t" + p2.DistanceFromSource().ToString());
+
+        Console.WriteLine("Point1 Distance From Point2: \t" + p1.DistanceFromPoint(p2).ToString());
     }
 }
