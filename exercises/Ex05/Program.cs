d462ceeebd5d148d7d63936c38a3d42d8a3bae5f
Ali <62478970+aliinium@users.noreply.github.com>
2024-08-02 12:47:02 +0330

    do ex05

--- exercises/Ex05/Program.cs
+++ exercises/Ex05/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex05;
+
+public class FifthExercise
+{
+    public static void Main(string[] args)
+    {
+        Ratio r1 = new Ratio();
+        r1.A = 2;
+        r1.B = 4;
+
+        Ratio r2 = new Ratio();
+        r2.A = 4;
+        r2.B = 8;
+
+        Console.Write("Ratio1 = ");
+        r1.Print();
+
+        Console.Write("Ratio2 = ");
+        r2.Print();
+
+
+        Ratio tmp = new Ratio();
+        tmp = r1.Add(r2);
+        Console.Write(r1.Show() + " + " + r2.Show() + " = ");
+        tmp.Print();
+
+        tmp = r1.Add(10);
+        Console.Write(r1.Show() + " + 10 = ");
+        tmp.Print();
+
+        tmp = r1.Mul(r2);
+        Console.Write(r1.Show() + " * " + r2.Show() + " = ");
+        tmp.Print();
+
+        tmp = r1.Mul(10);
+        Console.Write(r1.Show() + " * 10 = ");
+        tmp.Print();
+    }
+}
--- exercises/Ex05/Ratio.cs
+++ exercises/Ex05/Ratio.cs
@@ -1,6 +1,56 @@
-﻿namespace Ex05;
+﻿using System;
+using static System.Math;
+
+namespace Ex05;
 
 public class Ratio
 {
+    public int A;
+    public int B;
 
+    public Ratio Add(Ratio r) {
+        Ratio result = new Ratio();
+
+        result.B = this.B * r.B;
+        result.A = this.A * r.B + r.A * this.B;
+
+        return result;
+    }
+    public Ratio Add(int n) {
+        Ratio result = new Ratio();
+
+        result.B = this.B;
+        result.A = n * this.B + this.A;
+
+        return result;
+    }
+
+    public Ratio Mul(Ratio r)
+    {
+        Ratio result = new Ratio();
+
+        result.B = this.B * r.B;
+        result.A = this.A * r.A;
+
+        return result;
+    }
+    public Ratio Mul(int n)
+    {
+        Ratio result = new Ratio();
+
+        result.B = this.B;
+        result.A = n * this.A;
+
+        return result;
+    }
+
+    public void Print() 
+    {
+        Console.Write(this.A.ToString() + "/" + this.B.ToString() + "\n");
+    }
+    
+    public string Show()
+    {
+        return this.A.ToString() + "/" + this.B.ToString();
+    }
 }
