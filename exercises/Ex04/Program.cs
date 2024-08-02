e6d8de34dbd17974d51b096b763895fc949882c2
Ali <62478970+aliinium@users.noreply.github.com>
2024-08-02 12:26:30 +0330

    do ex04

--- exercises/Ex04/Program.cs
+++ exercises/Ex04/Program.cs
@@ -7,6 +7,28 @@ public class FourthExercise
 {
     public static void Main(string[] args)
     {
-        
+        Ratio r1 = new Ratio();
+        r1.A = 2;
+        r1.B = 4;
+
+        Ratio r2 = new Ratio();
+        r2.A = 8;
+        r2.B = 10;
+
+        Console.Write("Ratio1 = ");
+        r1.Print();
+
+        Console.Write("Ratio2 = ");
+        r2.Print();
+
+
+        Ratio tmp = new Ratio();
+        tmp = r1.Add(r2);
+        Console.Write("Ratio1 + Ratio2 = ");
+        tmp.Print();
+
+        tmp = r1.Mul(r2);
+        Console.Write("Ratio1 * Ratio2 = ");
+        tmp.Print();
     }
 }
--- exercises/Ex04/Ratio.cs
+++ exercises/Ex04/Ratio.cs
@@ -5,5 +5,30 @@ namespace Ex04;
 
 public class Ratio
 {
+    public int A;
+    public int B;
 
+    public Ratio Add(Ratio r) {
+        Ratio result = new Ratio();
+
+        result.B = this.B * r.B;
+        result.A = this.A * r.B + r.A * this.B;
+
+        return result;
+    }
+
+    public Ratio Mul(Ratio r)
+    {
+        Ratio result = new Ratio();
+
+        result.B = this.B * r.B;
+        result.A = this.A * r.A;
+
+        return result;
+    }
+
+    public void Print() 
+    {
+        Console.Write(this.A.ToString() + "/" + this.B.ToString() + "\n");
+    }
 }
